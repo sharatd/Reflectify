e9bb148dffa0aceb41628748886b39109384bf70
Sharat Dhananjaya <sharatdhananjaya@mac.lan>
2025-01-03 17:59:02 -0800

    resolved dependency issues related to copilot

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,15 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo"],
+    plugins: [
+      [
+        "module-resolver",
+        {
+          alias: {
+            "@": "./", // Matches the "paths" configuration in tsconfig.json
+          },
+        },
+      ],
+    ],
   };
 };
